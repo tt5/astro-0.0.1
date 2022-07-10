fead2e734e3c30a724386ed4bd46d5479b1c1246
n <2585803+tt5@users.noreply.github.com>
2022-07-10 22:54:18 +0200

    syntax highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'solarized-light',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+//    remarkplugins: [
+//      'remark-smartypants'
+//    ],
+//    rehypePlugins: [
+//    ],
+  }
+});
--- src/pages/posts/test.md
+++ src/pages/posts/test.md
@@ -1,7 +1,13 @@
 ---
 layout: '../../layouts/MdLayout.astro'
 title: 'My Markdown page'
+setup: |
+  import Title from '../../components/Title.astro'
 ---
-# Title
+<Title>Title</Title>
 
 This is my page, written in **Markdown.**
+
+```js
+console.log("hi");
+```
