6004a0c65f4e09b9bdeec12fc92662157cacf846
n <2585803+tt5@users.noreply.github.com>
2022-07-14 17:03:51 +0200

    add js route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
