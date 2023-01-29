5de54bcae04d8b326b9ebb876de2c793f366e2cd
n <2585803+tt5@users.noreply.github.com>
2023-01-29 19:46:26 +0100

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
-import rehypeKatex from 'rehype-katex'
-import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
@@ -12,21 +10,20 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/edge-functions";
+
+// https://astro.build/config
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
+    host: "0.0.0.0"
   },
-  adapter: cloudflare(),
+  output: "server",
+  adapter: netlify(),
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [
-      [
-        rehypeKatex,
-        {
-          output: "html",
-        },
-      ],
-    ],
+    rehypePlugins: [[rehypeKatex, {
+      output: "html"
+    }]],
     syntaxHighlight: "shiki",
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
@@ -37,9 +34,8 @@ export default defineConfig({
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
+      wrap: true
+    }
   },
-  integrations: [mdx()],
-  output: "server",
+  integrations: [mdx()]
 });
